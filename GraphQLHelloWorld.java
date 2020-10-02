60878e2af0a404dc1adbdcbc6d4b1087458949c8
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-10-02 11:34:10 +0530

    Initial Commit

---
+++ GraphQLHelloWorld.java
@@ -0,0 +1,14 @@
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+public class GraphQLHelloWorld {
+	public static void main(String[] args) {
+        
+    }
+}
